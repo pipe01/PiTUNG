634c16b8a5cffdadecf9da9f5b2062204b01a018
Pipe <pipernene@gmail.com>
2018-01-21 22:45:33 +0100

    Moved UI elements to a separate file

--- InternalPatches.cs
+++ InternalPatches.cs
@@ -1,9 +1,5 @@
 ﻿using Harmony;
-using PiTung_Bootstrap.Config_menu;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace PiTung_Bootstrap
@@ -69,74 +65,6 @@ namespace PiTung_Bootstrap
             {
                 item.OnGUI();
             }
-        }
-    }
-
-    internal interface IUiElement
-    {
-        void Draw();
-    }
-
-    internal struct UiLabel : IUiElement
-    {
-        private static Dictionary<Color, GUIStyle> ColorStyles = new Dictionary<Color, GUIStyle>();
-
-        public string Text { get; private set; }
-        public Vector2 Position { get; private set; }
-        public Color Color { get; private set; }
-
-        public UiLabel(string text, Vector2 position, Color color)
-        {
-            this.Text = text;
-            this.Position = position;
-            this.Color = color;
-        }
-
-        private GUIStyle GetColorStyle(Color color)
-        {
-            if (!ColorStyles.ContainsKey(color))
-            {
-                ColorStyles[color] = new GUIStyle
-                {
-                    normal = new GUIStyleState { textColor = color }
-                };
-            }
-
-            return ColorStyles[color];
-        }
-
-        public void Draw()
-        {
-            var style = GetColorStyle(this.Color);
-            var size = style.CalcSize(new GUIContent(this.Text));
-
-            GUI.Label(new Rect(this.Position, size), this.Text, style);
-        }
-    }
-
-    internal struct UiRect : IUiElement
-    {
-        public Rect Area { get; private set; }
-
-        private GUIStyle style;
-
-        public UiRect(Rect area, Color color)
-        {
-            this.Area = area;
-
-            Texture2D bg = new Texture2D(1, 1);
-            bg.SetPixel(0, 0, color);
-            bg.Apply();
-
-            this.style = new GUIStyle()
-            {
-                normal = new GUIStyleState { background = bg }
-            };
-        }
-
-        public void Draw()
-        {
-            GUI.Box(this.Area, "", this.style);
         }
     }
 }
