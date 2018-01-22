37d8cd959004249366c2fe81dd3f8087a73688fd
Pipe <pipernene@gmail.com>
2018-01-22 19:45:14 +0100

    Added test to check if mods load correctly

--- PiTung Bootstrap Tests/ModLoaderTest.cs
+++ PiTung Bootstrap Tests/ModLoaderTest.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PiTung_Bootstrap;
 
 namespace PiTung_Bootstrap_Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ModLoaderTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckIfAllModsAreLoaded()
         {
+            var mods = ModLoader.GetMods();
+
+            Assert.AreEqual(1, mods.Count());
+
+            if (mods.Any())
+            {
+                var mod = mods.First();
+
+                Assert.IsNotNull(mod.ModAssembly);
+                Assert.IsNotNull(mod.ModName);
+                Assert.IsNotNull(mod.ModAuthor);
+                Assert.IsNotNull(mod.ModVersion);
+            }
         }
     }
 }
