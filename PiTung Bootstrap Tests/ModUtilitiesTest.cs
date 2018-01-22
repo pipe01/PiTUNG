2f3b48415f2bd119269ccf1de2d05fb06c06b7cc
Pipe <pipernene@gmail.com>
2018-01-22 19:00:48 +0100

    Added some reflection tests

---
+++ PiTung Bootstrap Tests/ModUtilitiesTest.cs
@@ -0,0 +1,54 @@
+﻿using PiTung_Bootstrap;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PiTung_Bootstrap_Tests
+{
+    [TestClass]
+    public class ModUtilitiesTest
+    {
+        private class TestClass
+        {
+            public string PublicField = "hello";
+            private int PrivateField = 42;
+
+            public int GetPrivateField() => PrivateField;
+        }
+
+        [TestMethod]
+        public void SetPublicField()
+        {
+            var cls = new TestClass();
+
+            ModUtilities.SetFieldValue(cls, "PublicField", "goodbye", false);
+
+            Assert.AreEqual("goodbye", cls.PublicField);
+        }
+
+        [TestMethod]
+        public void SetPrivateField()
+        {
+            var cls = new TestClass();
+
+            ModUtilities.SetFieldValue(cls, "PrivateField", 21, true);
+
+            Assert.AreEqual(21, cls.GetPrivateField());
+        }
+
+        [TestMethod]
+        public void GetPublicField()
+        {
+            var cls = new TestClass();
+
+            Assert.AreEqual(cls.PublicField, ModUtilities.GetFieldValue<string>(cls, "PublicField", false));
+        }
+
+        [TestMethod]
+        public void GetPrivateField()
+        {
+            var cls = new TestClass();
+
+            Assert.AreEqual(cls.GetPrivateField(), ModUtilities.GetFieldValue<int>(cls, "PrivateField", true));
+        }
+    }
+}
