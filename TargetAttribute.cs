58de346ca9c4d7a5ce4f2b4b0e48940164b4f92b
Felipe Martínez <pipernene@gmail.com>
2018-01-26 19:44:16 +0100

    Docs fix

--- TargetAttribute.cs
+++ TargetAttribute.cs
@@ -9,8 +9,7 @@ namespace PiTung_Bootstrap
         /// Marks this class as a patch class. 
         /// <para />
         /// Patch classes may contains static methods in order to patch existing
-        /// ones, named in the following format: OriginalMethodName[Prefix|Postfix]. If no patch type 
-        /// (prefix or postfix) is specified, prefix will selected by default.
+        /// ones. If no patch type (prefix or postfix) is specified, prefix will selected by default.
         /// </summary>
         /// <param name="containerType">The type that contains the method we want to patch.</param>
         public TargetAttribute(Type containerType)
