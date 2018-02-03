0aa9cba8b7710ed6bb73c228013239b5d696d0d8
Felipe Martínez <pipernene@gmail.com>
2018-02-03 17:00:18 +0100

    Separate mod loading to a method

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -60,116 +60,7 @@ namespace PiTung_Bootstrap
 
             foreach (var mod in ModLoader.GetMods())
             {
-                if (LoadedMods.Contains(mod.FullPath))
-                {
-                    MDebug.WriteLine($"Skipping already loaded mod {mod.Name}.");
-                    continue;
-                }
-
-                if (!mod.Hotloadable && hotload)
-                {
-                    MDebug.WriteLine($"[WARNING] {mod.Name} can't be hotloaded.");
-                    continue;
-                }
-
-                if (mod.ModAssembly == null)
-                {
-                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: couldn't load assembly.");
-                    continue;
-                }
-
-                if (!mod.FrameworkVersion.EqualsVersion(PiTung.FrameworkVersion))
-                {
-                    if (mod.RequireFrameworkVersion)
-                    {
-                        MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: wrong PiTUNG version. Required version: {mod.FrameworkVersion}.");
-                        continue;
-                    }
-                    else
-                    {
-                        MDebug.WriteLine($"[WARNING] {mod.Name} may not work properly: wrong PiTUNG version. Optimal version: {mod.FrameworkVersion}.");
-                    }
-                }
-
-                try
-                {
-                    mod.BeforePatch();
-                }
-                catch (Exception ex)
-                {
-                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while executing before-patch method.");
-                    MDebug.WriteLine("More details: " + ex, 1);
-
-                    continue;
-                }
-
-                MDebug.WriteLine($"Loading {mod.FullName}...");
-
-                try
-                {
-                    _Harmony.PatchAll(mod.ModAssembly);
-
-                    foreach (Type cls in mod.ModAssembly.GetTypes())
-                    {
-                        var attrs = (TargetAttribute[])cls.GetCustomAttributes(typeof(TargetAttribute), false);
-
-                        if (attrs.Length == 0)
-                            continue;
-
-                        foreach (var patch in PatchUtilities.GetMethodPatches(cls, attrs[0].ContainerType))
-                        {
-                            var method = new HarmonyMethod(patch.PatchMethod);
-                            
-                            _Harmony.Patch(
-                                patch.BaseMethod,
-                                patch.Prefix ? method : null,
-                                patch.Postfix ? method : null);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while patching methods.");
-                    MDebug.WriteLine("More details: " + ex, 1);
-
-                    continue;
-                }
-
-                try
-                {
-                    mod.AfterPatch();
-                }
-                catch (Exception ex)
-                {
-                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while executing after-patch method.");
-                    MDebug.WriteLine("More details: " + ex, 1);
-
-                    continue;
-                }
-
-                MenuEntry[] entries;
-
-                try
-                {
-                    entries = mod.GetMenuEntries().ToArray();
-                }
-                catch (Exception)
-                {
-                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while creating menu entries.");
-
-                    continue;
-                }
-
-                if (entries.Length > 0)
-                {
-                    var entry = new TextMenuEntry { Text = mod.Name };
-                    entry.AddChildren(entries);
-
-                    ConfigMenu.Instance.Entries.Add(entry);
-                }
-
-                LoadedMods.Add(mod.FullPath);
-                MDebug.WriteLine($"{mod.Name} loaded successfully.");
+                LoadMod(mod, hotload);
             }
 
             MDebug.WriteLine("----------Done patching!----------");
@@ -180,6 +71,120 @@ namespace PiTung_Bootstrap
                     IGConsole.Log($"<color=#00ff00>PiTUNG version {v} available!</color> Run Installer.exe to update.");
             };
             ModUtilities.DummyComponent.StartCoroutine(UpdateChecker.CheckUpdates());
+        }
+
+        private void LoadMod(Mod mod, bool hotload)
+        {
+            if (LoadedMods.Contains(mod.FullPath))
+            {
+                MDebug.WriteLine($"Skipping already loaded mod {mod.Name}.");
+                return;
+            }
+
+            if (!mod.Hotloadable && hotload)
+            {
+                MDebug.WriteLine($"[WARNING] {mod.Name} can't be hotloaded.");
+                return;
+            }
+
+            if (mod.ModAssembly == null)
+            {
+                MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: couldn't load assembly.");
+                return;
+            }
+
+            if (!mod.FrameworkVersion.EqualsVersion(PiTung.FrameworkVersion))
+            {
+                if (mod.RequireFrameworkVersion)
+                {
+                    MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: wrong PiTUNG version. Required version: {mod.FrameworkVersion}.");
+                    return;
+                }
+                else
+                {
+                    MDebug.WriteLine($"[WARNING] {mod.Name} may not work properly: wrong PiTUNG version. Optimal version: {mod.FrameworkVersion}.");
+                }
+            }
+
+            try
+            {
+                mod.BeforePatch();
+            }
+            catch (Exception ex)
+            {
+                MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while executing before-patch method.");
+                MDebug.WriteLine("More details: " + ex, 1);
+
+                return;
+            }
+
+            MDebug.WriteLine($"Loading {mod.FullName}...");
+
+            try
+            {
+                _Harmony.PatchAll(mod.ModAssembly);
+
+                foreach (Type cls in mod.ModAssembly.GetTypes())
+                {
+                    var attrs = (TargetAttribute[])cls.GetCustomAttributes(typeof(TargetAttribute), false);
+
+                    if (attrs.Length == 0)
+                        continue;
+
+                    foreach (var patch in PatchUtilities.GetMethodPatches(cls, attrs[0].ContainerType))
+                    {
+                        var method = new HarmonyMethod(patch.PatchMethod);
+
+                        _Harmony.Patch(
+                            patch.BaseMethod,
+                            patch.Prefix ? method : null,
+                            patch.Postfix ? method : null);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while patching methods.");
+                MDebug.WriteLine("More details: " + ex, 1);
+
+                return;
+            }
+
+            try
+            {
+                mod.AfterPatch();
+            }
+            catch (Exception ex)
+            {
+                MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while executing after-patch method.");
+                MDebug.WriteLine("More details: " + ex, 1);
+
+                return;
+            }
+
+            MenuEntry[] entries;
+
+            try
+            {
+                entries = mod.GetMenuEntries().ToArray();
+            }
+            catch (Exception)
+            {
+                MDebug.WriteLine($"[ERROR] {mod.Name} failed to load: error while creating menu entries.");
+
+                return;
+            }
+
+            if (entries.Length > 0)
+            {
+                var entry = new TextMenuEntry { Text = mod.Name };
+                entry.AddChildren(entries);
+
+                ConfigMenu.Instance.Entries.Add(entry);
+            }
+
+            LoadedMods.Add(mod.FullPath);
+            MDebug.WriteLine($"{mod.Name} loaded successfully.");
         }
 
         /// <summary>
