41c46a7a608208e83286964552a94314bedfab26
Felipe Martínez <pipernene@gmail.com>
2018-02-13 18:38:15 +0100

    Added Create and Destroy sphere methods

--- Mod utilities/GraphicUtilities.cs
+++ Mod utilities/GraphicUtilities.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PiTung_Bootstrap
 {
@@ -7,6 +10,10 @@ namespace PiTung_Bootstrap
         internal GraphicUtilities() { }
 
         private static readonly ObjImporter _ObjImporter = new ObjImporter();
+
+        private int SphereCounter = 0;
+        private Dictionary<int, GameObject> Spheres = new Dictionary<int, GameObject>();
+        private Stack<GameObject> SphereCache = new Stack<GameObject>();
 
         /// <summary>
         /// Draws <paramref name="str"/> on screen at <paramref name="position"/> with color <paramref name="color"/>
@@ -42,6 +49,52 @@ namespace PiTung_Bootstrap
         public Mesh ImportMeshFromFile(string filePath)
         {
             return _ObjImporter.ImportFile(filePath);
+        }
+
+        /// <summary>
+        /// Creates a sphere at <paramref name="pos"/> with size <paramref name="size"/> and color <paramref name="color"/>.
+        /// </summary>
+        /// <param name="pos">The sphere's center.</param>
+        /// <param name="size">The sphere's size.</param>
+        /// <param name="color">The sphere's color.</param>
+        /// <returns>The sphere's ID. See <see cref="DestroySphere(int)"/>.</returns>
+        public int CreateSphere(Vector3 pos, float size = 1, Color? color = null)
+        {
+            GameObject obj;
+
+            if (SphereCache.Count == 0)
+            {
+                obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                Object.Destroy(obj.GetComponent<Collider>());
+            }
+            else
+            {
+                obj = SphereCache.Pop();
+            }
+
+            obj.transform.position = pos;
+            obj.transform.localScale = Vector3.one * (size * 0.1f);
+            obj.GetComponent<Renderer>().material.color = color ?? Color.green;
+            
+            int id = SphereCounter++;
+
+            Spheres.Add(id, obj);
+            return id;
+        }
+
+        /// <summary>
+        /// Destroys a sphere with ID <paramref name="id"/> previously spawned by <see cref="CreateSphere(Vector3, float, Color?)"/>.
+        /// </summary>
+        /// <param name="id">The ID of the sphere we want to destroy.</param>
+        public void DestroySphere(int id)
+        {
+            if (Spheres.TryGetValue(id, out var s))
+            {
+                s.SetActive(false);
+
+                SphereCache.Push(s);
+                Spheres.Remove(id);
+            }
         }
     }
 }
