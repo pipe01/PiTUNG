54f88851ca7a3e0cbad5a83bbf81e74f6e94020f
Felipe Martínez <pipernene@gmail.com>
2018-02-14 00:11:36 +0100

    Document

--- ModInput.cs
+++ ModInput.cs
@@ -13,6 +13,12 @@ namespace PiTung
 
         private static Dictionary<string, KeyCode> Binds = new Dictionary<string, KeyCode>();
         
+        /// <summary>
+        /// Registers a key binding for a mod. If the binding isn`t already loaded (it's not set in the file), <paramref name="defaultKey"/> will be the binded key.
+        /// </summary>
+        /// <param name="mod">The mod that is registering the key.</param>
+        /// <param name="name">The binding's name.</param>
+        /// <param name="defaultKey">The default binding key.</param>
         public static void RegisterBinding(Mod mod, string name, KeyCode defaultKey)
         {
             if (!Binds.ContainsKey(name))
@@ -22,6 +28,12 @@ namespace PiTung
             }
         }
 
+        /// <summary>
+        /// Gets the <see cref="KeyCode"/> for the binding with name <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">The name of the binding we want to get.</param>
+        /// <returns>The binding's key code.</returns>
+        /// <exception cref="KeyNotFoundException">Throws when a binding isn't found.</exception>
         public static KeyCode GetBindingKey(string name)
         {
             if (Binds.TryGetValue(name, out var k))
