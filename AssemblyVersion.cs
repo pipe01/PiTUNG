bcb13d08b4a6ed0a36dff8806590b4efc86ee894
Felipe Martínez <pipernene@gmail.com>
2018-02-15 16:51:35 +0100

    Version 1.5.2

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.1")]
-[assembly: AssemblyFileVersion("1.5.1")]
+[assembly: AssemblyVersion("1.5.2")]
+[assembly: AssemblyFileVersion("1.5.2")]
 
