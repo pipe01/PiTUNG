fd6a3e4d1af4c1b46178f0503ede1307e27fcb3a
Felipe Martínez <pipernene@gmail.com>
2018-03-09 17:11:47 +0100

    Version 1.6.4

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.4")]
+[assembly: AssemblyFileVersion("1.6.4")]
 
