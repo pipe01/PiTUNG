641c750ce69633ada8334d0c2ae88f9c4b9ef994
Felipe Martínez <pipernene@gmail.com>
2018-03-24 18:18:29 +0100

    Version 2.1.0

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
 
