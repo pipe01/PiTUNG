f51bc52c66c249b36899be715a112c3593140c96
Felipe Martínez <pipernene@gmail.com>
2018-03-30 18:15:59 +0200

    Version 2.3.1

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.3.1")]
+[assembly: AssemblyFileVersion("2.3.1")]
 
