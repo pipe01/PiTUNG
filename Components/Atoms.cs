bad2617d7aa0df3662aee50d8104a89718f0b7b6
Felipe Martínez <pipernene@gmail.com>
2018-03-31 00:24:28 +0200

    Moved atoms to their own file

---
+++ Components/Atoms.cs
@@ -0,0 +1,69 @@
+﻿using References;
+using System;
+using UnityEngine;
+
+namespace PiTung.Components
+{
+    internal interface IAtom
+    {
+        void AddToGameObject(GameObject obj);
+    }
+
+    internal abstract class IOAtom : IAtom
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+
+        public abstract void AddToGameObject(GameObject obj);
+    }
+    internal sealed class InputPegAtom : IOAtom
+    {
+        public override void AddToGameObject(GameObject obj)
+        {
+            BuilderUtils.AddInputPeg(obj, this.Position).transform.localRotation = this.Rotation;
+        }
+    }
+    internal sealed class OutputAtom : IOAtom
+    {
+        public override void AddToGameObject(GameObject obj)
+        {
+            BuilderUtils.AddOutputPeg(obj, this.Position).transform.localRotation = this.Rotation;
+        }
+    }
+    internal sealed class IOMapAtom : IAtom
+    {
+        public IOMap Map { get; set; }
+
+        public void AddToGameObject(GameObject obj)
+        {
+            BuilderUtils.ApplyIOMap(obj, Map);
+        }
+    }
+
+    internal interface IStructureAtom
+    {
+        GameObject GetRootObject();
+    }
+
+    internal sealed class CubeStructureAtom : IStructureAtom
+    {
+        public GameObject GetRootObject()
+        {
+            return GameObject.Instantiate(Prefabs.WhiteCube, new Vector3(-1000, -1000, -1000), Quaternion.identity);
+        }
+    }
+    internal sealed class CustomStructureAtom : IStructureAtom
+    {
+        public Func<GameObject> Root { get; }
+
+        public CustomStructureAtom(Func<GameObject> root)
+        {
+            this.Root = root;
+        }
+
+        public GameObject GetRootObject()
+        {
+            return this.Root();
+        }
+    }
+}
