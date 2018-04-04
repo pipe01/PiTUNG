1cd4bf357cfd69bed455d25275e89be6e889e6e0
Stenodyon <stenodyon@gmail.com>
2018-04-04 18:24:59 +0200

    Added autocompletion

---
+++ Console/Autocompletion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiTung.Console
+{
+    static class Autocompletion
+    {
+        public static IEnumerable<String> Candidates(String command, IEnumerable<String> commands)
+        {
+            return commands.Where(candidate => candidate.StartsWith(command));
+        }
+    }
+}
