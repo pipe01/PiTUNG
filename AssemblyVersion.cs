9b3b92a6b65541500a66034bff8ad55e1ec93b7c
Felipe Martínez <pipernene@gmail.com>
2018-04-07 16:24:53 +0200

    Version 2.3.10

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.9")]
-[assembly: AssemblyFileVersion("2.3.9")]
+[assembly: AssemblyVersion("2.3.10")]
+[assembly: AssemblyFileVersion("2.3.10")]
 
