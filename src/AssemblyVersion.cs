a2335886fd44de58a652c698a2921b2a0e5625a2
Felipe Martínez <pipernene@gmail.com>
2018-04-12 20:20:51 +0200

    Version 2.3.11

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.10")]
-[assembly: AssemblyFileVersion("2.3.10")]
+[assembly: AssemblyVersion("2.3.11")]
+[assembly: AssemblyFileVersion("2.3.11")]
 
