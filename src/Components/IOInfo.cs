82944de0869516ac2350e6654d519a17e35ba492
Felipe Martínez <pipernene@gmail.com>
2018-06-01 19:32:02 +0200

    Forgot to add this

---
+++ src/Components/IOInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PiTung.Components
+{
+    internal class IOInfo : MonoBehaviour
+    {
+        public int Index { get; set; }
+    }
+}
