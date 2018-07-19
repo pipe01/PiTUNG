9bcc46062ff9eede912c661c8817d4f999cc4c71
Felipe Martínez <pipernene@gmail.com>
2018-07-19 20:38:44 +0200

    Version 2.4.1

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyFileVersion("2.4.1")]
 
